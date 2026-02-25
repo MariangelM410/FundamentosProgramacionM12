8c7b6956b6aaf4be14adadb1ecade8b1d072cbac
MariangelM410 <mariangel.mejia@upb.edu.co>
2026-02-25 13:35:57 -0500

    clase de 23 de feb

---
+++ 2.CondicionalesSimples/2.CondicionalesSimples/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2.CondicionalesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*       string nombre = "";
+                   float sueldo = 0;
+                   Console.WriteLine("Ingrese el nombre del usuario");
+                   nombre = Console.ReadLine(); //Lo que llega desde la consola, es de tipo string
+                   Console.WriteLine("Ingrese el sueldo del usuario");
+                   sueldo = Convert.ToSingle(Console.ReadLine());
+
+                   if (sueldo >= 3000)
+
+                       Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+
+               }*/
+        }
+     
+            
+ 
+}
+
+      
