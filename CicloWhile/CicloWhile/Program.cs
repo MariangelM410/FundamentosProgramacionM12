134017081e3897e512a0e06d6b5a086a0f9f3b5a
MariangelM410 <mariangel.mejia@upb.edu.co>
2026-03-11 13:30:21 -0500

    Add files via upload

---
+++ CicloWhile/CicloWhile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acumulador = 0;
+
+            while (contador <=5 )
+            {
+                acumulador += contador;
+                Console.WriteLine($"Contador: {contador} - Acumulador: {acumulador}");
+                contador++;
+            }
+
+            Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");
+        
+        }
+
+    }
+}
