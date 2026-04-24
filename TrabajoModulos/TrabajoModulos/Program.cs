03ab8c22a5e5d66825f31189f024894a20f349be
MariangelM410 <mariangel.mejia@upb.edu.co>
2026-04-24 13:40:14 -0500

    Add files via upload

---
+++ TrabajoModulos/TrabajoModulos/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajoModulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMenú()
+
+            int opcion = CapturaOpcion();
+            RealizarOperacion(opcion);
+        }
+
+        static void MostrarMenú()
+        {
+            Console.WriteLine("----------------------Mostrar Menú----------------------");
+            Console.WriteLine("    1.Suma                                2.Resta");
+            Console.WriteLine("    3. Multiplicación                     4. División");
+            Console.WriteLine("--------------------------------------------------------");
+        }
+
+        static int CapturaOpcion()
+        {
+            Console.WriteLine("Ingrese una opción del menú");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void RealizarOperacion(int opcion)
+        {
+            switch (opcion)
+            {
+                case 1:
+                    Suma();
+                    break;
+                case 2:
+                    Resta();
+                    break;
+                case 3:
+                    Multiplicacion();
+                    break;
+                case 4:
+                    Division();
+                    break;
+                default:
+                    Console.WriteLine("Ingrese una opción válida deñ menú");
+                    break;
+            }
+
+            private static void Division()
+        {
+            Console.WriteLine("Usted va a dividir");
+        }
+
+        private static void Multiplicacion()
+        {
+            Console.WriteLine("Usted va a dividir");
+        }
+
+        private static void Resta()
+        {
+            Console.WriteLine("Usted va a dividir");
+        }
+
+        private static void Suma()
+        {
+            Console.WriteLine("Usted va a dividir");
+        }
+
+    }
+}
+}
